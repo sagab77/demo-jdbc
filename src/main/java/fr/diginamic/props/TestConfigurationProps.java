059f71023c5f0aac7a6f6a8ae0e39de587aa6520
sagab77 <64429763+sagab77@users.noreply.github.com>
2021-06-09 11:11:07 +0200

    Initial commit

---
+++ src/main/java/fr/diginamic/props/TestConfigurationProps.java
@@ -0,0 +1,13 @@
+package fr.diginamic.props;
+
+import java.util.ResourceBundle;
+
+public class TestConfigurationProps {
+    public static void main(String[] args) {
+
+        ResourceBundle config = ResourceBundle.getBundle("database");
+
+
+
+    }
+}
